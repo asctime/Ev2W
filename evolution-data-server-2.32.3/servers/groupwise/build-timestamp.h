7b6ff381103a654882c40694a76b26408234d62d
asctime <you@example.com>
2023-02-26 01:12:45 +1100

    Google account removed from this release (outdated-notworking) All gdata dependencies removed. All gnome dependecies removed (except gnome-icon-theme and gnome-vfs)

--- evolution-data-server-2.32.3/servers/groupwise/build-timestamp.h
+++ evolution-data-server-2.32.3/servers/groupwise/build-timestamp.h
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 const char *build_timestamp = "Evolution-Data-Server-2.32-2023-03-20";
+=======
+const char *build_timestamp = "Evolution-Data-Server-2.32-2023-02-25";
+>>>>>>> 1975953... Google account removed from this release (outdated-notworking)
