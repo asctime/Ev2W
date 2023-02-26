97715f85eb01eba12bbc480d44d9382eeda0b5ec
asctime <you@example.com>
2023-02-27 05:11:53 +1100

    Further gnome-desktop references removed, refactor to latest Ev2W.

--- evolution-exchange-2.32.3/mingw64/config.h
+++ evolution-exchange-2.32.3/mingw64/config.h
@@ -5,7 +5,11 @@
 #define BASE_VERSION "2.32"
 
 /* Used to prevent clock-setting attacks against pilot licenses */
+<<<<<<< HEAD
 #define E2K_APPROX_BUILD_TIME 1677283200
+=======
+#define E2K_APPROX_BUILD_TIME 1676937600
+>>>>>>> 2aa22ab... Further gnome-desktop references removed, refactor to latest Ev2W.
 
 /* Define if you want E2K_DEBUG to be available */
 /* #undef E2K_DEBUG */
